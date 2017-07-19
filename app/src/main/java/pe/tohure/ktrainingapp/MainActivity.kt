99760db9a42e74d3a282001e38818a8598cb240b
Carlo Renzo Huamán Torres <tohure.gec@gmail.com>
2017-07-19 01:27:35 -0500

    First Launch in Kotlin

---
+++ app/src/main/java/pe/tohure/ktrainingapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package pe.tohure.ktrainingapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+
+        btnClick.setOnClickListener {
+            val textLbl = lblName.text
+            Toast.makeText(this,textLbl,Toast.LENGTH_SHORT).show()
+        }
+    }
+}
